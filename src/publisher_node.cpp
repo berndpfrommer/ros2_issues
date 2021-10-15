32af103aabd616b2a9bb605a73a14a8c2f91a245
Morgan Quigley <morgan@osrfoundation.org>
2021-10-15 14:26:22 +0800

    column-major style message and a C++ subscriber
    Signed-off-by: Morgan Quigley <morgan@osrfoundation.org>

--- src/publisher_node.cpp
+++ src/publisher_node.cpp
@@ -17,6 +17,7 @@
 #include <unistd.h>
 
 #include <rclcpp/rclcpp.hpp>
+#include <ros2_issues/msg/test_array_column_major.hpp>
 #include <ros2_issues/msg/test_array_complex.hpp>
 #include <ros2_issues/msg/test_array_simple.hpp>
 #include <thread>
@@ -57,12 +58,60 @@ struct TestPublisher : public rclcpp::Node
   std::thread thread_;
 };
 
+template <class MsgType>
+struct TestPublisherColumnMajor : public rclcpp::Node
+{
+  explicit TestPublisherColumnMajor(const rclcpp::NodeOptions & options)
+  : Node("test_publisher", options)
+  {
+    pub_ = create_publisher<MsgType>(
+      "~/array", declare_parameter<int>("q_size", 1000));
+    thread_ = std::thread([this]() {
+      rclcpp::Rate rate(declare_parameter<int>("rate", 1000));
+      const int numElements = declare_parameter<int>("num_elements", 100);
+      rclcpp::Time t_start = now();
+      size_t msg_cnt(0);
+      const rclcpp::Duration logInterval = rclcpp::Duration::from_seconds(1.0);
+      MsgType msg;
+      msg.x.resize(numElements);
+      msg.y.resize(numElements);
+      //msg.ts.resize(numElements);
+      msg.secs.resize(numElements);
+      msg.nsecs.resize(numElements);
+      msg.polarity.resize(numElements);
+
+      while (rclcpp::ok()) {
+        msg.header.stamp = now();
+        pub_->publish(msg);
+        rate.sleep();
+        msg_cnt++;
+        rclcpp::Time t = now();
+        const rclcpp::Duration dt = t - t_start;
+        if (dt > logInterval) {
+          RCLCPP_INFO(get_logger(), "pub rate: %8.2f", msg_cnt / dt.seconds());
+          t_start = t;
+          msg_cnt = 0;
+        }
+      }
+    });
+  }
+  // -- variables
+  typename rclcpp::Publisher<MsgType>::SharedPtr pub_;
+  std::thread thread_;
+};
+
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
   if ((argc > 1) && std::string(argv[1]) == "-s") {
     auto node =
       std::make_shared<TestPublisher<ros2_issues::msg::TestArraySimple>>(
+        rclcpp::NodeOptions());
+    rclcpp::spin(node);
+  } 
+  else if ((argc > 1) && std::string(argv[1]) == "-c") {
+    auto node =
+      std::make_shared<TestPublisherColumnMajor<ros2_issues::msg::TestArrayColumnMajor>>(
         rclcpp::NodeOptions());
     rclcpp::spin(node);
   } else {
